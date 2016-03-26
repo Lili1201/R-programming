b6c7d357fac28a33bf8a1bf6e18411dcc9d3919c
Lili1201 <lilianavillaorduna@gmail.com>
2016-03-26 17:15:07 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,20 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+## x is sample size
+	n<-(sqrt(x))
+	m<<-matrix(sample(x),n,n)
+	m
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(...) {
+	d<<-det(m)
+	if(d != 0){
+	        i<<-solve(m)
+	        }	
+	i
 }
+        ## Return a matrix that is the inverse of 'x'
